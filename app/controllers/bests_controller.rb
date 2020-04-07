0baa364c24d5d76c0d49fe3623d24d76a229ddec
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-04-07 00:53:40 +0000

    Create best(new,create,index,show,edit,update,destroy #6

---
+++ app/controllers/bests_controller.rb
@@ -0,0 +1,43 @@
+class BestsController < ApplicationController
+  def new
+    @best = Best.new
+  end
+
+  def create
+    @best = Best.new(best_params)
+    @best.user_id = current_user.id
+    @best.save
+    redirect_to bests_path
+  end
+
+  def index
+    @bests = Best.all
+  end
+
+  def show
+    @best = Best.find(params[:id])
+    @user = @best.user
+  end
+
+  def edit
+    @best = Best.find(params[:id])
+  end
+
+  def update
+    @best = Best.find(params[:id])
+    @best.update(best_params)
+    redirect_to best_path(@best.id)
+  end
+
+  def destroy
+    @best = Best.find(params[:id])
+    @best.destroy
+    redirect_to bests_path
+  end
+
+  private
+
+  def best_params
+    params.require(:best).permit(:best_image, :best_name, :best_url, :genre, :recommend)
+  end
+end

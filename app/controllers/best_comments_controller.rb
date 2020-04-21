7b5f949ca4db879407f686a904376a770faa74d4
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-04-21 04:35:48 +0000

    Add ajax_features,pre-view_features #24

--- app/controllers/best_comments_controller.rb
+++ app/controllers/best_comments_controller.rb
@@ -4,13 +4,12 @@ class BestCommentsController < ApplicationController
     @best_comment = @best.best_comments.new(best_comment_params)
     @best_comment.user_id = current_user.id
     @best_comment.save
-    redirect_back(fallback_location: best_path(@best))
   end
 
   def destroy
     @best_comment = BestComment.find(params[:best_id])
+    @best = @best_comment.best
     @best_comment.destroy
-    redirect_back(fallback_location: bests_path)
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    @best = Best.find(params[:best_id])
     favorite = current_user.favorites.build(best_id: params[:best_id])
     favorite.save
-    redirect_back(fallback_location: bests_path)
   end
 
   def destroy
+    @best = Best.find(params[:best_id])
     favorite = Favorite.find_by(best_id: params[:best_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: bests_path)
   end
 end

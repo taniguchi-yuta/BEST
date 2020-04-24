72a89fce436b542abc0d8c1fbc24b60afe6486ab
taniguchi-yuta <w.08035781696.crip@gmail.com>
2020-04-24 09:58:21 +0000

    Debug,Improve best_show,Add paginate #30

--- app/models/best.rb
+++ app/models/best.rb
@@ -1,8 +1,8 @@
 class Best < ApplicationRecord
   attachment :best_image
   belongs_to :user
-  has_many :favorites
-  has_many :best_comments
+  has_many :favorites, dependent: :destroy
+  has_many :best_comments, dependent: :destroy
   enum genre: { BEST_buy: 0, BEST_go: 1, BEST_use: 2 }
   validates :best_name, presence: true
 
